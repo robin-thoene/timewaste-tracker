403ce8ace8498447fb8f6c4339a8d6c5a3d893fc
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2024-02-25 12:56:10 +0100

    update packages

--- next-app/hooks/useRecentPlayerPlaytimes/useRecentPlayerPlaytimes.tsx
+++ next-app/hooks/useRecentPlayerPlaytimes/useRecentPlayerPlaytimes.tsx
@@ -4,21 +4,23 @@ import { IRecentPlayerPlaytime } from '../../types';
 
 /**
  * Query the recent player playtimes.
- *
  * @returns {IRecentPlayerPlaytime[]} The recent player playtimes.
  */
 const useRecentPlayerPlaytimes = () => {
-    return useQuery(['recent-player-playtimes'], async () => {
-        try {
-            const response = await fetch('/api/recent-player-playtimes');
-            const data = (await response.json()) as IRecentPlayerPlaytime[];
-            data.forEach((d) => {
-                d.recentPlaytimeInHours = Math.round(d.recentPlaytimeInHours);
-            });
-            return data;
-        } catch (error) {
-            console.error(error);
-        }
+    return useQuery({
+        queryKey: ['recent-player-playtimes'],
+        queryFn: async () => {
+            try {
+                const response = await fetch('/api/recent-player-playtimes');
+                const data = (await response.json()) as IRecentPlayerPlaytime[];
+                data.forEach((d) => {
+                    d.recentPlaytimeInHours = Math.round(d.recentPlaytimeInHours);
+                });
+                return data;
+            } catch (error) {
+                console.error(error);
+            }
+        },
     });
 };
 
--- readme.md
+++ readme.md
@@ -3,3 +3,10 @@
 ## About
 
 Simple app to visualize the time a group of people spent in games based on their steam statistics.
+
+## Local development
+
+To locally develop the app, you need to first create a `.env.local` file inside the `./next-app` directory. Inside that file you need to add the following environment variables:
+
+PLAYERS_JSON: The array of player objects that defines which player profiles you want to track (see the [model](./next-app/types/player.ts) for reference).
+STEAM_API_KEY: Your api key to access the Steam api.
