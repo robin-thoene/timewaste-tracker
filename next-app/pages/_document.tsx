3081fda0468cb32c90c5f25e43b048d79fb2ea01
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2025-04-26 10:38:54 +0200

    feat: add new favicon

--- next-app/pages/_document.tsx
+++ next-app/pages/_document.tsx
@@ -9,10 +9,13 @@ export default function Document(): JSX.Element {
     return (
         <Html>
             <Head>
-                <link rel="shortcut icon" href="/favicon.ico" />
                 <meta name="robots" content="index, follow" />
                 <meta name="description" content="This is the templated NextJS app for the project timewaste-tracker." />
                 <meta property="og:title" content="timewaste-tracker" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="96x96" href="/img/favicon/favicon-96x96.png" />
+                <link rel="icon" type="image/png" sizes="256x256" href="/img/favicon/favicon-256x256.png" />
             </Head>
             <body>
                 <Main />
