ab6c34e875fb6485e431836fca04e953179f06e2
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2025-06-14 00:12:13 +0200

    fix: build

--- next-app/instrumentation.ts
+++ next-app/instrumentation.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line jsdoc/require-jsdoc
 export async function register() {
     if (process.env.NEXT_RUNTIME === 'nodejs') {
         await import('./instrumentation.otel.ts');
